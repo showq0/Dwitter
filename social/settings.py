755ba93acc1ffc141db790874d8d8572236f296c
showq0 <showqbalawneh@gmail.com>
2025-05-21 19:51:27 +0300

    host to deploy

--- social/settings.py
+++ social/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ["dwitterapp.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["dwitter-latest.onrender.com","dwitterapp.onrender.com", "127.0.0.1"]
 
 # Application definition
 
